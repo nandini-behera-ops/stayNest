be83b2707231fa7a779177f07eba693979a6e7a5
Nandini Behera <nandinibehera2025@gmail.com>
2026-03-30 18:10:21 +0530

    delete route is working

--- app.js
+++ app.js
@@ -61,6 +61,13 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
 app.listen(8080, (req, res) => {
   console.log("server listening to port 8080");
 });
