d6c8189078aa75b244e53904e7ac44b75dd3b1e7
Nandini Behera <nandinibehera2025@gmail.com>
2026-03-30 19:28:57 +0530

    navbar footer set

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
@@ -13,6 +14,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 main()
   .then(() => {
     console.log("connected to DB");
