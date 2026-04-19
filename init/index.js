c02e7ce0cf84150aaef40e93ed2cbedf8de622bb
Nandini Behera <nandinibehera2025@gmail.com>
2026-04-19 20:39:11 +0530

    edit dlt autho done

--- init/index.js
+++ init/index.js
@@ -10,11 +10,16 @@ main()
     console.log("connected to DB");
   })
   .catch((err) => {
-    console.log(er);
+    console.log(err);
   });
 
 const initDB = async (req, res) => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: new mongoose.Types.ObjectId("69e4cd4d10943e7f0b570892"),
+    // owner: "69e4cd4d10943e7f0b570892",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was intialized");
 };
