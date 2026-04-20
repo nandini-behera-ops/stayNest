4a63c9623c9e9da190a46dfcbafa231024b83056
Nandini Behera <nandinibehera2025@gmail.com>
2026-04-20 14:56:44 +0530

    uploading image

--- models/listing.js
+++ models/listing.js
@@ -9,13 +9,8 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdGVsfGVufDB8fDB8fHww",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdGVsfGVufDB8fDB8fHww"
-        : v,
+    url: String,
+    filename: String,
   },
   price: Number,
   location: String,
--- routes/listing.js
+++ routes/listing.js
@@ -4,14 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
-router
-  .route("/")
-  .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing),
-  );
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+router.route("/").get(wrapAsync(listingController.index)).post(
+  isLoggedIn,
+
+  upload.single("listing[image]"),
+  validateListing,
+  wrapAsync(listingController.createListing),
+);
+
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 router
   .route("/:id")
@@ -23,7 +26,6 @@ router
     wrapAsync(listingController.updateListing),
   )
   .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
-//New Route
 
 //edit route
 router.get(
